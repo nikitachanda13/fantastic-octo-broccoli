3cd9c3ca3503c0d52d7bc75075e2ee54abacf07d
nikitachanda13 <nikitachanda@NikitaChandas-MacBook-Air.local>
2025-04-30 19:03:21 +0530

    Initial commit

---
+++ code.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    //int marks[5]={99,100,54,36,88};
+   /*marks[0]=202;
+     cout<<marks[0]<<endl;
+     cout<<marks[1]<<endl;
+     cout<<marks[2]<<endl;
+     cout<<marks[3]<<endl;
+     cout<<marks[4]<<endl;*/ 
+     int size=5;
+     int marks[size];
+     for(int i=0; i<size;i++)
+    {
+        cout<<marks[i] << endl;
+    }
+    //  int sz=sizeof(marks)/
+    // cout<< sizeof(marks)/sizeof(int) << endl;
+    //loops
+    for(int i=0; i<size;i++)
+    {
+        cout<<marks[i] << endl;
+    }
+
+    return 0;
+}
